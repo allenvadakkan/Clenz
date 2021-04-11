29bf239fcf6d50b2e72afb95416c10a11220f32a
Allen <allenjose99@gmail.com>
2021-04-11 13:13:19 +0530

    Firstcommit

---
+++ client/Angularecommerce/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  stripeKey: '82496e8a-ed50-4709-8b81-20689824304d'
+};
---
+++ server/config.js
@@ -0,0 +1,9 @@
+//All the configurations required like portNumber, Database Connection URl etc are stored to provide a low level encapsulation
+
+module.exports = {
+  database:
+    'mongodb+srv://cleaning:HKBQjyv0xnfLDafj@cluster0.yvqkp.mongodb.net/test',
+  port: 3030,
+  secret: 'RamtejaRepaka2012312321'
+};
+
